a73cd70ca7987242b3743132b19695d97e8a32f5
Felipeee07 <parraoscar663@gmail.com>
2025-09-25 09:35:52 -0500

    Update Comentario.java

--- Comentario.java
+++ Comentario.java
@@ -1,13 +1,26 @@
 package co.edu.poli.actividad3.model;
 
-// 🔹 Clase que no se puede heredar
+/**
+ * Clase final que representa un comentario hecho a una receta.
+ * Esta clase no puede ser heredada.
+ * Contiene información sobre el autor, fecha y calificación del comentario.
+ * 
+ * @author Felipe Parra
+ */
 public final class Comentario {
     private String idComentario;
     private String autor;
-    private String fecha;       
+    private String fecha;
     private double calificacion;
 
-    
+    /**
+     * Constructor para crear un comentario con sus atributos.
+     * 
+     * @param idComentario Identificador único del comentario.
+     * @param autor Nombre del autor del comentario.
+     * @param fecha Fecha en la que se hizo el comentario.
+     * @param calificacion Calificación numérica asociada al comentario.
+     */
     public Comentario(String idComentario, String autor, String fecha, double calificacion) {
         this.idComentario = idComentario;
         this.autor = autor;
@@ -15,39 +28,83 @@ public final class Comentario {
         this.calificacion = calificacion;
     }
 
+    /**
+     * Obtiene el identificador del comentario.
+     * 
+     * @return id del comentario.
+     */
     public String getIdComentario() {
         return idComentario;
     }
 
+    /**
+     * Establece el identificador del comentario.
+     * 
+     * @param idComentario Nuevo id del comentario.
+     */
     public void setIdComentario(String idComentario) {
         this.idComentario = idComentario;
     }
 
+    /**
+     * Obtiene el nombre del autor del comentario.
+     * 
+     * @return autor del comentario.
+     */
     public String getAutor() {
         return autor;
     }
 
+    /**
+     * Establece el nombre del autor del comentario.
+     * 
+     * @param autor Nuevo nombre del autor.
+     */
     public void setAutor(String autor) {
         this.autor = autor;
     }
 
+    /**
+     * Obtiene la fecha del comentario.
+     * 
+     * @return fecha del comentario.
+     */
     public String getFecha() {
         return fecha;
     }
 
+    /**
+     * Establece la fecha del comentario.
+     * 
+     * @param fecha Nueva fecha del comentario.
+     */
     public void setFecha(String fecha) {
         this.fecha = fecha;
     }
 
+    /**
+     * Obtiene la calificación asociada al comentario.
+     * 
+     * @return calificación numérica.
+     */
     public double getCalificacion() {
         return calificacion;
     }
 
+    /**
+     * Establece la calificación del comentario.
+     * 
+     * @param calificacion Nueva calificación numérica.
+     */
     public void setCalificacion(double calificacion) {
         this.calificacion = calificacion;
     }
 
-    // 🔹 toString
+    /**
+     * Representación en String del objeto Comentario.
+     * 
+     * @return Cadena con la información del comentario.
+     */
     @Override
     public String toString() {
         return "Comentario{" +
